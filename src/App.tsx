573f970fde7a69a3f770c537300ac80ff7be39f6
Kadmo <kadmohardy@gmail.com>
2021-04-05 22:50:07 -0300

    feat(home): create routes and new page named home

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,19 @@
 import { StylesProvider } from '@material-ui/core/styles';
 import React from 'react';
+import { BrowserRouter as Router } from 'react-router-dom';
 import { ThemeProvider } from 'styled-components';
+import Routes from './routes';
 import theme from './styles/theme';
 
 const App: React.FC = () => {
   return (
-    <StylesProvider injectFirst>
-      <ThemeProvider theme={theme}>
-        <div>Hello World</div>
-      </ThemeProvider>
-    </StylesProvider>
+    <Router>
+      <StylesProvider injectFirst>
+        <ThemeProvider theme={theme}>
+          <Routes />
+        </ThemeProvider>
+      </StylesProvider>
+    </Router>
   );
 };
 
