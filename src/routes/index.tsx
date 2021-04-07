142fb48ae970de7db02f5a9c8e881d9d9d53dfb6
Kadmo <kadmohardy@gmail.com>
2021-04-06 21:43:43 -0300

    feat(users): create sign screeen

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -2,12 +2,14 @@ import React from 'react';
 import { Route, Switch } from 'react-router-dom';
 import Home from '../pages/Home';
 import SignUp from '../pages/SignUp';
+import SignIn from '../pages/SignIn';
 
 const Routes: React.FC = () => {
   return (
     <Switch>
       <Route path="/" exact component={Home} />
       <Route path="/signup" exact component={SignUp} />
+      <Route path="/signin" exact component={SignIn} />
     </Switch>
   );
 };
