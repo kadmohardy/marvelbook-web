ddb66027642ebacf5d4297608105f323249ce440
Kadmo <kadmohardy@gmail.com>
2021-04-06 23:04:01 -0300

    feat(redux): add redux wrapper to App

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,26 @@
 import { StylesProvider } from '@material-ui/core/styles';
 import React from 'react';
 import { BrowserRouter as Router } from 'react-router-dom';
+import { Provider } from 'react-redux';
+import { PersistGate } from 'redux-persist/integration/react';
+
 import { ThemeProvider } from 'styled-components';
 import Routes from './routes';
 import theme from './styles/theme';
+import { store, persistor } from './store';
 
 const App: React.FC = () => {
   return (
     <Router>
-      <StylesProvider injectFirst>
-        <ThemeProvider theme={theme}>
-          <Routes />
-        </ThemeProvider>
-      </StylesProvider>
+      <Provider store={store}>
+        <PersistGate persistor={persistor}>
+          <StylesProvider injectFirst>
+            <ThemeProvider theme={theme}>
+              <Routes />
+            </ThemeProvider>
+          </StylesProvider>
+        </PersistGate>
+      </Provider>
     </Router>
   );
 };
